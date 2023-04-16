a444085ba227edee4c8d84b8572a51d9de68806f
techming <hmeng29@wisc.edu>
2023-04-16 14:13:01 -0500

    chore: adding react-hook-form and refactor code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,36 @@
+import { useEffect } from "react";
 import Head from "next/head";
+import { useForm, Controller } from "react-hook-form";
 import { Flex, Heading, Text } from "@chakra-ui/layout";
 import { Box, Button, Image, Input, SimpleGrid } from "@chakra-ui/react";
-import Navigation from "@/components/general/Navigation";
+import Navigation from "@/components/module/Navigation/Navigation";
 import styles from "@/styles/Home.module.css";
 
+interface LoginData {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
 export default function Home() {
+  const {
+    control,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+  });
+  const onSubmit = (data: LoginData) => console.log(data);
+
+  useEffect(() => {
+    console.log(errors);
+  }, [errors]);
+
   return (
     <>
       <Head>
@@ -49,17 +75,56 @@ export default function Home() {
                 Become a Hero Today
               </Text>
               <SimpleGrid columns={2} mt={4} mb={4} spacing={2}>
-                <Input variant={"filled"} placeholder="First Name" />
-                <Input variant={"filled"} placeholder="Last Name" />
+                <Controller
+                  name="firstName"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <Input
+                      variant={"filled"}
+                      placeholder="First Name"
+                      isInvalid={!!errors?.firstName}
+                      {...field}
+                    />
+                  )}
+                />
+
+                <Controller
+                  name="lastName"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <Input
+                      variant={"filled"}
+                      placeholder="Last Name"
+                      isInvalid={!!errors?.lastName}
+                      {...field}
+                    />
+                  )}
+                />
               </SimpleGrid>
               <Flex>
-                <Input
-                  variant={"filled"}
-                  placeholder="Email"
-                  mr={2}
-                  type="email"
+                <Controller
+                  name="email"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <Input
+                      variant={"filled"}
+                      placeholder="Email"
+                      type="email"
+                      mr={2}
+                      isInvalid={!!errors?.email}
+                      {...field}
+                    />
+                  )}
                 />
-                <Button minW={"30%"} bg={"brand.dark.800"} color={"white"}>
+                <Button
+                  minW={"30%"}
+                  bg={"brand.dark.800"}
+                  color={"white"}
+                  onClick={handleSubmit(onSubmit)}
+                >
                   Get Started
                 </Button>
               </Flex>
